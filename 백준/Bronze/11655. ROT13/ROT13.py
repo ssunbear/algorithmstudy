f44e7362ce078b95edd4710d28fb3a2145a6fb4e
ssunbear <117508164+ssunbear@users.noreply.github.com>
2022-11-21 22:53:46 +0900

    [Bronze I] Title: ROT13, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,10 @@
+import sys
+
+string=sys.stdin.readline()
+for i in string:
+    if i.islower():
+        print(chr(97+(ord(i)+13-97)%26), end='')
+    elif i.isupper():
+        print(chr(65+(ord(i)+13-65)%26), end='')
+    else:
+        print(i,end='')
