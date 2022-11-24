8e2db62ba77045374a4a67a3cc04562bfcf24ce5
ssunbear <117508164+ssunbear@users.noreply.github.com>
2022-11-24 22:25:22 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 868 ms, Memory: 118164 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,16 +1,17 @@
-import sys
+#Counter는 리스트를 값으로 주게 되면 해당 원소들이 몇 번 등장했는지 세어 딕셔너리 형태로 반환
+#https://hongcoding.tistory.com/12
+from collections import Counter
 from sys import stdin
-from bisect import bisect_left, bisect_right
-n=int(sys.stdin.readline())
-data1=list(map(int, sys.stdin.readline().split()))
-m=int(sys.stdin.readline())
-data2=list(map(int, sys.stdin.readline().split()))
-data1.sort()
 
-def countbyrange(arr, value):
-    rightidx=bisect_right(arr, value)
-    leftidx=bisect_left(arr,value)
-    return rightidx-leftidx
+n = stdin.readline().rstrip()
+card = list(map(int,stdin.readline().split()))
+m = stdin.readline().rstrip()
+test = list(map(int,stdin.readline().split()))
 
-for i in range(len(data2)):
-    print(countbyrange(data1,data2[i]), end=' ')
+count = Counter(card)
+
+for i in range(len(test)):
+    if test[i] in count:
+        print(count[test[i]], end=' ')
+    else:
+        print(0, end=' ')
